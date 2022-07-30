9e102b735333051af420396c984ae542a2c9bfb9
simontrinh0911 <93080579+simontrinh0911@users.noreply.github.com>
2022-07-30 23:52:58 +0200

    Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import PackageDescription
+PackageDescription
 
-let package = Package(
+  package  Package(
   name: "GoogleTagManager",
   platforms: [.iOS(.v11)],
   products: [
