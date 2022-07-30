570ee59a4c05d43e76eba988caa5800b3d8c1baf
simontrinh0911 <93080579+simontrinh0911@users.noreply.github.com>
2022-07-31 01:06:32 +0200

    Package.swift
    Fix

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- PackageDescription
- package  Package(
+  PackageDescription
+  package  Package(
   name: "GoogleTagManager",
   platforms: [.iOS(.v11)],
   products: [
